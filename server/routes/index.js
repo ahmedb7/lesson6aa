2255c9a597e33545e7ae3cade27d2c292e532f5f
Benzir Ahmed <benzir.ahmed7@gmail.com>
2017-02-24 21:22:46 -0500

    first commit

---
+++ server/routes/index.js
@@ -0,0 +1,39 @@
+// modules required for routing
+let express = require('express');
+let router = express.Router();
+let mongoose = require('mongoose');
+let passport = require('passport');
+
+// define the user model
+let UserModel = require('../models/users');
+let User = UserModel.User; // alias for User
+
+// define the game model
+let game = require('../models/games');
+
+// function to check if the user is authenticated
+function requireAuth(req, res, next) {
+  // check if the user is logged index
+  if(!req.isAuthenticated()) {
+    return res.redirect('auth/login');
+  }
+  next();
+}
+
+/* GET home page. wildcard */
+router.get('/', (req, res, next) => {
+  res.render('content/index', {
+    title: 'Home',
+    games: ''
+   });
+});
+
+/* GET contact page. */
+router.get('/contact', (req, res, next) => {
+  res.render('content/contact', {
+    title: 'Contact',
+    games: ''
+   });
+});
+
+module.exports = router;
